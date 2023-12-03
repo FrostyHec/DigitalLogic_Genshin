986fda09e6fcc16773c1da5951b8ca4f3d666fb3
Frosty <3259024151@qq.com>
2023-12-03 18:28:21 +0800

    Implemented State Module.
    Signed-off-by: Frosty <3259024151@qq.com>

--- HDL_Framework/GenshinKitchen.srcs/sources_1/new/TargetStateEncoder.v
+++ HDL_Framework/GenshinKitchen.srcs/sources_1/new/TargetStateEncoder.v
@@ -23,8 +23,22 @@
 
 module TargetStateEncoder(
 input [5:0] state,
-input enable,
-output [7:0] tx,
-output activation
+input input_activate,
+output reg [7:0] tx,
+output reg activation
     );
+    reg [5:0] sender_data;
+    reg[1:0] sender_channel;  
+    always @(input_activate) begin
+        if(input_activate) begin
+            sender_data=state;
+            sender_channel=`Sender_Channel_TargetMachineChanged;
+            activation=1'b1;
+        end else begin
+            sender_data=`Sender_Data_Ignore;
+            sender_channel=`Sender_Channel_Ignore;
+            activation=1'b0;    
+        end
+        tx={sender_data,sender_channel};
+    end
 endmodule
